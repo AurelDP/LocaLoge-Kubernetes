479b4c0dfa1fcebd4fc1d7522e1281b5f088a9ff
nelson <nelson.amorim.branco@efrei.net>
2023-11-30 14:08:18 +0100

    [Fix] fixed Swagger and Spring version for reservationService

--- server/reservationService/src/main/java/com/localoge/reservationService/controller/ReservationController.java
+++ server/reservationService/src/main/java/com/localoge/reservationService/controller/ReservationController.java
@@ -3,10 +3,11 @@ package com.localoge.reservationService.controller;
 import com.localoge.reservationService.exceptions.Exceptions;
 import com.localoge.reservationService.model.Reservation;
 import com.localoge.reservationService.service.IReservationService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,65 +21,59 @@ public class ReservationController {
     @Autowired
     private IReservationService reservationService;
 
-    @ApiOperation(value = "Create a reservation", response = Reservation.class)
-    @ApiParam(name = "reservation", value = "The reservation to create", required = true)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully created the reservation"),
-            @ApiResponse(code = 404, message = "Rental not found"),
-            @ApiResponse(code = 409, message = "The reservation already exists"),
-            @ApiResponse(code = 500, message = "Internal server error")
+    @Operation(summary = "Create a reservation", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully created reservation", content = @Content(schema = @Schema(implementation = Reservation.class))),
+            @ApiResponse(responseCode = "404", description = "Rental not found"),
+            @ApiResponse(responseCode = "409", description = "Reservation already exists"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
     @PostMapping("/create")
-    public Reservation createReservation(@RequestBody Reservation reservation) throws Exception {
+    public Reservation createReservation(@Parameter(description = "The reservation to create", required = true) @RequestBody Reservation reservation) throws Exception {
         return reservationService.createReservation(reservation);
     }
 
-    @ApiOperation(value = "Delete a reservation", response = Reservation.class)
-    @ApiParam(name = "id", value = "Reservation id", required = true)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully deleted the reservation"),
-            @ApiResponse(code = 404, message = "Reservation not found"),
-            @ApiResponse(code = 500, message = "Internal server error")
+    @Operation(summary = "Delete a reservation", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted reservation", content = @Content(schema = @Schema(implementation = void.class))),
+            @ApiResponse(responseCode = "404", description = "Reservation not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
     @PostMapping("/delete")
-    public void deleteReservation(@RequestParam Integer id) throws Exceptions.ReservationNotFoundException {
+    public void deleteReservation(@Parameter(description = "Reservation id", required = true) @RequestParam Integer id) throws Exceptions.ReservationNotFoundException {
         reservationService.deleteReservation(id);
     }
 
-    @ApiOperation(value = "Get a reservation", response = Reservation.class)
-    @ApiParam(name = "id", value = "Reservation id", required = true)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully get the reservation"),
-            @ApiResponse(code = 404, message = "Reservation not found"),
-            @ApiResponse(code = 500, message = "Internal server error")
+    @Operation(summary = "Get a reservation", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved reservation", content = @Content(schema = @Schema(implementation = Reservation.class))),
+            @ApiResponse(responseCode = "404", description = "Reservation not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
     @GetMapping("/get")
-    public Reservation getReservation(@RequestParam Integer id) throws Exceptions.ReservationNotFoundException {
+    public Reservation getReservation(@Parameter(description = "Reservation id", required = true) @RequestParam Integer id) throws Exceptions.ReservationNotFoundException {
         return reservationService.getReservation(id);
     }
 
-    @ApiOperation(value = "Get reservation by rentalId", response = Reservation.class)
-    @ApiParam(name = "rentalId", value = "Rental id", required = true)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully get the reservation by the rental id"),
-            @ApiResponse(code = 404, message = "Reservation not found"),
-            @ApiResponse(code = 500, message = "Internal server error")
+    @Operation(summary = "Get reservation by rentalId", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully get the reservation by the rental id", content = @Content(schema = @Schema(implementation = Reservation.class))),
+            @ApiResponse(responseCode = "404", description = "Reservation not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
     @GetMapping("/rental")
-    public Reservation getReservationByRentalId(@RequestParam Integer rentalId) throws Exceptions.ReservationNotFoundException {
+    public Reservation getReservationByRentalId(@Parameter(description = "Rental id", required = true) @RequestParam Integer rentalId) throws Exceptions.ReservationNotFoundException {
         return reservationService.getReservationByRentalId(rentalId);
     }
 
-    @ApiOperation(value = "Get all reservations", response = Reservation.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully get all reservations"),
-            @ApiResponse(code = 500, message = "Internal server error"),
-            @ApiResponse(code = 500, message = "Internal server error")
+    @Operation(summary = "Get all reservations", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all reservations", content = @Content(schema = @Schema(implementation = List.class))),
+            @ApiResponse(responseCode = "404", description = "No reservations found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
     @GetMapping("/getall")
     public List<Reservation> getReservations() throws Exceptions.ReservationNotFoundException {
         return reservationService.getReservations();
     }
-
-
 }
--- server/reservationService/src/main/java/com/localoge/reservationService/model/Reservation.java
+++ server/reservationService/src/main/java/com/localoge/reservationService/model/Reservation.java
@@ -1,11 +1,11 @@
 package com.localoge.reservationService.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -15,23 +15,23 @@ public class Reservation {
     @Id
     @GeneratedValue
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    @ApiModelProperty(notes = "The database generated reservation ID", required = true)
+    @Schema(description = "The database generated reservation ID")
     private Integer id;
 
     @NotNull
-    @ApiModelProperty(notes = "The id of the rental that is being reserved", required = true)
+    @Schema(description = "The id of the rental that is being reserved")
     private Integer rentalId;
 
     @NotNull
-    @ApiModelProperty(notes = "The name of the person who made the reservation", required = true)
+    @Schema(description = "The name of the person who made the reservation")
     private String name;
 
     @NotNull
-    @ApiModelProperty(notes="The email of the person who made the reservation", required = true)
+    @Schema(description="The email of the person who made the reservation")
     private String email;
 
     @NotNull
-    @ApiModelProperty(notes="The phone number of the person who made the reservation", required = true)
+    @Schema(description="The phone number of the person who made the reservation")
     private String phone;
 
 }
