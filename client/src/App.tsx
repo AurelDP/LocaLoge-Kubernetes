08d2b29077d0e5a9b7a1743ea0d0fd48d5b13690
Aurélien <aureliendp@free.fr>
2024-01-10 12:19:52 +0100

    [Feat] Added database connection in each service (not finished)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,14 @@
 import './App.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHouse, faMoneyBill, faLock} from "@fortawesome/free-solid-svg-icons";
-import React, {useState} from "react";
 
 function App() {
 
-  const [cloudChecked, setCloudChecked] = useState(false);
   const swaggerBase = "http://localhost";
   const swaggerRoute = "/swagger-ui/index.html";
 
-  const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCloudChecked(event.target.checked);
-  }
-
-  const openSwagger = (servicePath: string, localPort: string) => {
-    if (!cloudChecked) {
-      window.open(swaggerBase + ":" + localPort + swaggerRoute, "_blank");
-    } else {
+  const openSwagger = (servicePath: string) => {
       window.open(swaggerBase + servicePath + swaggerRoute, "_blank");
-    }
   }
 
   return (
@@ -26,30 +16,18 @@ function App() {
       <div className="header">
         <img src="../public/assets/logo_white.png" alt="LocaLoge logo" className="logo"/>
       </div>
-      <div className="selector">
-        <h3 className={cloudChecked ? "" : "h3-checked"}>
-          Local
-        </h3>
-        <label className="switch">
-          <input type="checkbox" onChange={handleCheck}/>
-          <span className="slider round"></span>
-        </label>
-        <h3 className={cloudChecked ? "h3-checked" : ""}>
-          Cloud
-        </h3>
-      </div>
       <div className="buttons">
-        <div className="card" onClick={() => openSwagger("/housing", "8080")}>
+        <div className="card" onClick={() => openSwagger("/housing")}>
           <FontAwesomeIcon icon={faHouse} size="2x"/>
           <h2>Housing Service</h2>
           <p>Access the Housing Service Swagger</p>
         </div>
-        <div className="card" onClick={() => openSwagger("rental", "8081")}>
+        <div className="card" onClick={() => openSwagger("/rental")}>
           <FontAwesomeIcon icon={faMoneyBill} size="2x"/>
           <h2>Rental Service</h2>
           <p>Access the Rental Service Swagger</p>
         </div>
-        <div className="card" onClick={() => openSwagger("/reservation", "8082")}>
+        <div className="card" onClick={() => openSwagger("/reservation")}>
           <FontAwesomeIcon icon={faLock} size="2x"/>
           <h2>Reservation Service</h2>
           <p>Access the Reservation Service Swagger</p>
